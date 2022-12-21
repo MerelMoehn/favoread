c88b27f88d59285bad9e4c1df00076323428f39c
MerelMoehn <merelmoehn@outlook.com>
2022-12-21 13:16:44 +0000

    Add admin book approval functionality

--- bookcases/admin.py
+++ bookcases/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Book
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'author', 'approved', 'created_on')
+    search_fields = ['title', 'author']
+    list_filter = ('approved', 'created_on')
+    actions = ['approve_books']
+
+    def approve_books(self, request, queryset):
+        queryset.update(approved=True)
